2022d8c74b0d1d9ac49a28f70db71a3fb1924d46
Bruno <brunomessiasflores@gmail.com>
2023-03-29 18:17:57 -0300

    Exercicio 3

--- dia-2-javascript/exercicio3.js
+++ dia-2-javascript/exercicio3.js
@@ -1,7 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0
 let media = 0
 
 for (let index = 0; index < numbers.length; index += 1) {
-    media += numbers[index]
-    console.log(sum)
+    sum += numbers[index]
+    media = sum / 3
 }
+
+console.log(media)
