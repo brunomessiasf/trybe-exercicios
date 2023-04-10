9fea9aa2bbcf4af368381934769c7c57b8795d18
Bruno <brunomessiasflores@gmail.com>
2023-04-10 15:27:39 -0300

    Pratica 1

---
+++ dia-1-javascript-DOM/pratica1.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+      const title = document.getElementById("page-title").innerText = 'Jurassic Park 2';
+      const secondParagraph = document.getElementById("second-paragraph").innerText = 'Filme sobre dinossauros';
+      const subtitle = document.getElementById("subtitle").innerText = 'O reino perdido';
