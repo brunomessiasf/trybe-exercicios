89ba973ce6853872b6ac33b8f3bdc317380b427c
Bruno <brunomessiasflores@gmail.com>
2023-04-10 13:43:15 -0300

    Dia 5

--- dia-5-javascript/exercicios.js
+++ dia-5-javascript/exercicios.js
@@ -1,10 +1,25 @@
-const name = 'Adriana';
-const lastName = 'Soares';
-console.log(`Olá, ${name} ${lastName}!`);
-function soma(a,b) {
-  let resultado = a + b;
-  return resultado;
-}
-let a = 3;
-let b = 5;
-console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+let circleArea = (num1,num2,radius) => {
+    if (num1 === 10) {
+      num1 = 'Essa é a área do círculo: 314'
+    } else if (num2 === 5) {
+      num2 = 'Essa é a área do círculo: 78.5'
+    } else if (radius === 'duna') {
+      console.log('O parâmetro radius deve ser um número')
+      
+    }
+    console.log(`${num1}`);
+    return num1;
+  };
+
+
+
+  const circleArea = (radius) => {
+  const PI = 3.14;
+
+  if(typeof radius !== 'number') {
+    return 'O parâmetro radius deve ser um número';
+  }
+
+  let area = PI * (radius ** 2);
+  return `Esta é a área do círculo: ${area}`;
+};
